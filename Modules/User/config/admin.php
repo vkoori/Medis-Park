97b0ec3408c355c00db46fcfcfb30e00820af2c5
Koorosh SafeAshrafi <k.ashrafi@dariahamrah.ir>
2025-08-14 17:34:57 +0330

    fix: default admins

--- Modules/User/config/admin.php
+++ Modules/User/config/admin.php
@@ -3,17 +3,21 @@
 use libphonenumber\PhoneNumberUtil;
 use Modules\User\Support\FormattedPhoneNumber;
 
-$superAdmins = env('SUPER_ADMIN_MOBILE', '');
-$superAdmins = explode(',', $superAdmins);
-$superAdmins = array_map(
-    callback: fn($superAdmin): FormattedPhoneNumber => new FormattedPhoneNumber(
-        phoneNumber: PhoneNumberUtil::getInstance()->parse(
-            numberToParse: trim($superAdmin),
-            defaultRegion: 'IR'
-        )
-    ),
-    array: $superAdmins
-);
+$superAdmins = env('SUPER_ADMIN_MOBILE');
+if (!empty($superAdmins)) {
+    $superAdmins = explode(',', $superAdmins);
+    $superAdmins = array_map(
+        callback: fn($superAdmin): FormattedPhoneNumber => new FormattedPhoneNumber(
+            phoneNumber: PhoneNumberUtil::getInstance()->parse(
+                numberToParse: trim($superAdmin),
+                defaultRegion: 'IR'
+            )
+        ),
+        array: $superAdmins
+    );
+} else {
+    $superAdmins = [];
+}
 
 return [
     'super_admins' => $superAdmins,
