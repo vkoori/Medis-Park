69b825936eda1f4cde68ba1fa764348e136debe6
Koorosh SafeAshrafi <k.ashrafi@dariahamrah.ir>
2025-08-15 11:04:56 +0330

    feat: visible sms on staging

--- Modules/Notification/routes/v1/general.php
+++ Modules/Notification/routes/v1/general.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 Route::get('/sms', function () {
-    if (!app()->isLocal()) {
+    if (!app()->isLocal() && !app()->environment('staging')) {
         throw new RouteNotFoundException();
     }
 
